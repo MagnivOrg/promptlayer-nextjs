957cb72b20838985381993506e9b9961b6f5a798
AbuBakar <abubakarsohail.dev@gmail.com>
2024-04-26 11:21:50 +0500

    Update API route to remove unused code and console.log

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,5 @@
 import BaseAnthropic from "@anthropic-ai/sdk";
 import { promptlayer } from "promptlayer";
-promptlayer.api_key = process.env.PROMPTLAYER_API_KEY;
 const Anthropic: typeof BaseAnthropic = promptlayer.Anthropic;
 const anthropic = new Anthropic();
 export const POST = async () => {
@@ -14,6 +13,5 @@ export const POST = async () => {
     max_tokens: 100,
     model: "claude-3-sonnet-20240229",
   });
-  console.log(process.env.PROMPTLAYER_API_KEY);
   return Response.json(response.content[0].text);
 };
